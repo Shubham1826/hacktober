ac9309fdb0beaa6bf5188511b292302bd3e78aba
Anu sri <anusri.kola22@gmail.com>
2021-10-24 16:32:03 +0530

    series

--- series.c
+++ series.c
@@ -1,50 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
+//Fibonacci series
 
-// function to compute and print the nth term of the sequence
-void ComputeTerm(int n)
-{
-	// ak_3 stores the ak-3th term, ak_2 the ak-2th term, ak_1 the ak-1th 
-	// term and ak the kth term
-	int ak_3 = 1, ak_2 = 2, ak_1 = 3, ak;
-
-	if(n == 1)
-		ak = 1;
-	else if(n == 2)
-		ak = 2;
-	else if(n == 3)
-		ak = 3;
-	else
-	{
-		for(int k=4; k<=n; k++)
-		{
-			ak = ak_2 + 2 * ak_3;
-
-			// updating the values of ak_3, ak_2, ak_1
-			ak_3 = ak_2;
-			ak_2 = ak_1;
-			ak_1 = ak;
-		}
-	}
-
-	printf("The nth term of the sequence is %d.\n", ak);
-
-	return;
-}
+#include<stdio.h>
 
 int main()
 {
-	printf("Please enter n.\n");
-    int n;
-    scanf("%d", &n);
+	int n,i;
+	int n1=0,n2=1; //initializing 1st two terms
+	int n3 = n1 + n2; //initializing 3rd term(i.e,sum of the previous 2 terms)
 
-    if(n < 1)
-    {
-    	printf("Invalid Input: The value of n cannot be less than 1.\n");
-    	return 0;
-    }
+	printf("Enter the number : "); //taking input from user
+	scanf("%d",&n);
 
-    ComputeTerm(n);
+	printf("Fibonacci series : %d %d ", n1, n2); //printing 1st two terms
 
-    return 0;
+    //for printing 3rd to n terms
+	for (int i = 3; i <= n; ++i)
+	{
+		printf("%d ",n3);
+		n1 = n2;
+		n2 = n3;
+		n3 = n1+n2;
+	}
+	
+	return 0;
+	
 }
